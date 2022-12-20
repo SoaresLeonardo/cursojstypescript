7ac343612148d295f3860c9c4883109dc46d543f
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-20 11:01:15 -0300

    A0028

---
+++ src/A0028-constructor-privado/A0028-constructor-privado.ts
@@ -0,0 +1,22 @@
+export class Database {
+  private static database: Database;
+
+  constructor(
+    private host: string,
+    private user: string,
+    private password: string,
+  ) {}
+
+  connect(): void {
+    console.log(`Conectado: ${this.host}, ${this.user}, ${this.password}`);
+  }
+
+  static getDatabase(host: string, user: string, password: string): Database {
+    if (Database.database) return Database.database;
+    Database.database = new Database(host, user, password);
+    return Database.database;
+  }
+}
+
+const db1 = new Database('localhost', 'root', '12345');
+db1.connect();
