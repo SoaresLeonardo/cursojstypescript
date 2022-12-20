a4bfcb3feb054d59aebebfeb7b2c17c4013ca927
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-20 11:01:28 -0300

    A0030

---
+++ src/A0030-associacao/A0030-associacao.ts
@@ -0,0 +1,52 @@
+export class Escritor {
+  private _ferramenta: Ferramenta | null = null;
+
+  constructor(private _nome: string) {}
+
+  get nome(): string {
+    return this._nome;
+  }
+
+  set ferramenta(ferramenta: Ferramenta | null) {
+    this._ferramenta = ferramenta;
+  }
+
+  get ferramenta(): Ferramenta | null {
+    return this._ferramenta;
+  }
+
+  escrever(): void {
+    if (this._ferramenta === null) {
+      console.log('Não posso escrever sem ferramenta... ');
+    }
+  }
+}
+
+export abstract class Ferramenta {
+  constructor(private _nome: string) {}
+  abstract escrever(): void;
+
+  get nome(): string {
+    return this._nome;
+  }
+}
+
+export class Caneta extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está escrevendo...`);
+  }
+}
+
+export class MaquinaEscrever extends Ferramenta {
+  escrever(): void {
+    console.log(`${this.nome} está digitando...`);
+  }
+}
+
+const escritor = new Escritor('Leonardo');
+const caneta = new Caneta('Bic');
+const maquinaEscrever = new MaquinaEscrever('Bic');
+
+console.log(escritor.nome);
+console.log(caneta.nome);
+console.log(maquinaEscrever.nome);
