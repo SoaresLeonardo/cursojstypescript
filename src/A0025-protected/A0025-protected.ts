5127d40bd8b589e8998ff349dd3cb9e4e138cbb1
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-20 11:00:57 -0300

    A0025

---
+++ src/A0025-protected/A0025-protected.ts
@@ -0,0 +1,44 @@
+export class Empresa {
+  public readonly nome: string;
+  protected readonly colaboradores: Colaborador[] = [];
+  private readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  adicionaColaboradores(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaboradores(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Udemy extends Empresa {
+  constructor() {
+    super('Udemy', '11.111.111/0001-11');
+  }
+
+  pop(): Colaborador | null {
+    const colaborador = this.colaboradores.pop();
+
+    if (colaborador) return colaborador;
+    return null;
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string,
+  ) {}
+}
+
+const empresa1 = new Udemy();
+console.log(empresa1.nome);
+console.log(empresa1);
