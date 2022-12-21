bcb06ae4aaa9651e2e15cbeabb2b2bf191283a0f
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-20 21:01:20 -0300

    A0037

---
+++ src/A0037-type-guards/A0037-type-guards.ts
@@ -0,0 +1,19 @@
+function add(a: unknown, b: unknown): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+type Pessoa = { nome: string };
+type Animal = { cor: string };
+type PessoaOuAnimal = Pessoa | Animal;
+
+export class Aluno implements Pessoa {
+  constructor(public nome: string) {}
+}
+
+function mostraNome(obj: PessoaOuAnimal): void {
+  // if ('nome' in obj) console.log(obj.nome);
+  if (obj instanceof Aluno) console.log(obj.nome);
+}
+
+mostraNome(new Aluno('Joao'));
