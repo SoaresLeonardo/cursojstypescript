f310c1a36e81b34848722259627bf319ae57e6d2
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-21 08:22:42 -0300

    A0038

--- src/A0038-keyof-typeof/A0038-keyof-typeof.ts
+++ src/A0038-keyof-typeof/A0038-keyof-typeof.ts
@@ -5,10 +5,11 @@ const coresObj = {
   vermelho: 'red',
   verde: 'green',
   azul: 'blue',
+  roxo: 'purple',
 };
 
 function traduzirCor(cor: CoresChaves, cores: CoresObj) {
   return cores[cor];
 }
 
-console.log(traduzirCor('vermelho', coresObj));
+console.log(traduzirCor('verde', coresObj));
