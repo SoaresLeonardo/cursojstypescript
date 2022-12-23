2d8d289f579f6104277d6c6c0aad849c9b7a963d
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-23 09:01:50 -0300

    Create module.ts

---
+++ src/A0059-namespaces/module.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace MeuNomespace {
+  const nome = 'Leonardo';
+
+  export class PessoaDoNamespace {
+    constructor(public name: string) {}
+  }
+
+  // const pessoa = new PessoaDoNamespace('Leonardo');
+  // console.log(pessoa);
+}
