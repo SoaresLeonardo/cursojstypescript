1ddca77ccc49765a73f90f21543b7b9d1e3c3415
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-23 09:01:33 -0300

    A0057

---
+++ src/A0057-decorator-property/A0057-decorator-property.ts
@@ -0,0 +1,33 @@
+function decorador(classProperty: any, nome: string | symbol): any {
+  console.log(classProperty);
+  console.log(nome);
+}
+
+export class UmaPessoa {
+  @decorador
+  nome: string;
+  sobrenome: string;
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  method(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
