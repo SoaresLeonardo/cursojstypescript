1df1c132774b90ed2694f07f7ec1cba62bdf7250
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-23 09:01:01 -0300

    A0051

---
+++ src/A0051-decorator-class/A0051-decorator-class.ts
@@ -0,0 +1,17 @@
+export class Animal {
+  constructor(public cor: string) {}
+}
+
+function decorator<T extends new (...args: any[]) => any>(target: T): T {
+  return class extends target {
+    cor: string;
+    constructor(...args: any) {
+      super(...args);
+      this.cor = 'Qualquer coisa';
+    }
+  };
+}
+
+const AnimalDecorated = decorator(Animal);
+const animal = new AnimalDecorated('roxo');
+console.log(animal);
